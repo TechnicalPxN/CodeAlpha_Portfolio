3136b98001205a539bad1ac0d7e7025f909c65ec
Bhagwat Nagare <141098047+TechnicalPxN@users.noreply.github.com>
2026-03-11 18:54:48 +0530

    Add typing effect and navigation link handlers
    Implement typing effect for text and add click event listeners for navigation links.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const text=["Frontend Developer","Web Designer","JavaScript Developer"]
+
+let count=0
+let index=0
+let currentText=""
+let letter=""
+
+function type(){
+
+if(count===text.length){
+count=0
+}
+
+currentText=text[count]
+
+letter=currentText.slice(0,++index)
+
+document.getElementById("typing").textContent=letter
+
+if(letter.length===currentText.length){
+
+count++
+index=0
+
+setTimeout(type,1000)
+
+}else{
+
+setTimeout(type,100)
+
+}
+
+}
+
+type()
+
 document.querySelectorAll("nav a").forEach(link=>{
 
 link.addEventListener("click",e=>{
